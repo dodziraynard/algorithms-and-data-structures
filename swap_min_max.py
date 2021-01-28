2b0020364c0cb01631c202300e19bbe279960cf1
Raynard Dodzi Helegah <45526474+dodziraynard@users.noreply.github.com>
2021-01-28 18:22:36 +0000

    implemented swap_min_max function

--- swap_min_max.py
+++ swap_min_max.py
@@ -1,13 +1,13 @@
 def swap_min_max(arr: list):
-    # Find the maximal element in this list
+    # Find the maximal element in the list
     max = sorted(arr)[-1]
-    # Find the minimal element in this list
+    # Find the minimal element in the list
     min = sorted(arr, reverse=True)[-1]
 
     index_max = arr.index(max)
     index_min = arr.index(min)
 
-    # Swap the the position of min and the max
+    # Swap the the position of min and the max element
     arr[index_max] = min
     arr[index_min] = max
     print(arr)
